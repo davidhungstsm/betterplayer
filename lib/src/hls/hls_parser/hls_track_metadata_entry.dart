3f9d116fa98e85db65400b81096c6dc953c232af
davidhungstsm <41978612+davidhungstsm@users.noreply.github.com>
2025-08-15 17:25:16 +0700

    Update hls_track_metadata_entry.dart

--- lib/src/hls/hls_parser/hls_track_metadata_entry.dart
+++ lib/src/hls/hls_parser/hls_track_metadata_entry.dart
@@ -28,5 +28,9 @@ class HlsTrackMetadataEntry {
   }
 
   @override
-  int get hashCode => hashValues(groupId, name, variantInfos);
+  int get hashCode => Object.hash(
+    groupId,
+    name,
+    variantInfos,
+  );
 }
